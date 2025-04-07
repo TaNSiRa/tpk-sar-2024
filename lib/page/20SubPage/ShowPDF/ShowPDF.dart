eef206c9116c289514bd9dc5c5956ed39b36f7bd
TaNSiRa <tansirawit2543@gmail.com>
2025-04-07 17:19:34 +0700

    .

--- lib/page/20SubPage/ShowPDF/ShowPDF.dart
+++ lib/page/20SubPage/ShowPDF/ShowPDF.dart
@@ -38,104 +38,108 @@ void showPDF(String pdf64, BuildContext context) {
               },
               child: Container(child: Text('CLOSE')),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text(
-                    (requestData[0].sentRepStatus == null ||
-                            requestData[0].sentRepStatus == "")
-                        ? "NOT SENT YET"
-                        : requestData[0].sentRepStatus.toString(),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: (requestData[0].sentRepStatus == null ||
+            if (userName == 'N.KRITTAPAT' ||
+                userName == 'K.WORACHAI' ||
+                userName == 'R.TRIMATE' ||
+                userName == 'P.RATCHANEE')
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text(
+                      (requestData[0].sentRepStatus == null ||
                               requestData[0].sentRepStatus == "")
-                          ? Colors.red
-                          : Colors.green,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                if (requestData[0].sentRepStatus != 'SENT COMPLETE' &&
-                    requestData[0].incharge == userName) ...[
-                  Container(
-                    child: IconButton(
-                      padding: EdgeInsets.zero,
-                      iconSize: 22,
-                      icon: Icon(
-                        Icons.send_rounded,
-                        color: Colors.black,
+                          ? "NOT SENT YET"
+                          : requestData[0].sentRepStatus.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: (requestData[0].sentRepStatus == null ||
+                                requestData[0].sentRepStatus == "")
+                            ? Colors.red
+                            : Colors.green,
                       ),
-                      onPressed: () {
-                        CoolAlert.show(
-                          barrierDismissible: false,
-                          width: 200,
-                          showCancelBtn: true,
-                          context: context,
-                          type: CoolAlertType.custom,
-                          text: 'SENT REPORT TO CUSTOMER',
-                          confirmBtnText: 'Yes',
-                          cancelBtnText: 'No',
-                          loopAnimation: false,
-                          onConfirmBtnTap: () {
-                            DateTime now = new DateTime.now();
-                            print(now.toString());
-                            print(sampleData[0]);
-                            sentReportData.clear();
-                            sentReportData.add(sampleData[0]);
-                            sentReportData[0].sentRep = now.toString();
-                            sentReportData[0].sentRepStatus = "SENT REPORT";
-                            SentReport();
-                          },
-                          onCancelBtnTap: () {},
-                        );
-                      },
                     ),
                   ),
-                ],
-                if (requestData[0].sentRepStatus.toString() ==
-                        "SENT COMPLETE" &&
-                    requestData[0].incharge == userName) ...[
-                  Container(
-                    child: IconButton(
-                      padding: EdgeInsets.zero,
-                      icon: Icon(
-                        Icons.cancel,
-                        color: Colors.red,
+                  SizedBox(
+                    width: 10,
+                  ),
+                  if (requestData[0].sentRepStatus != 'SENT COMPLETE' &&
+                      requestData[0].incharge == userName) ...[
+                    Container(
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        iconSize: 22,
+                        icon: Icon(
+                          Icons.send_rounded,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          CoolAlert.show(
+                            barrierDismissible: false,
+                            width: 200,
+                            showCancelBtn: true,
+                            context: context,
+                            type: CoolAlertType.custom,
+                            text: 'SENT REPORT TO CUSTOMER',
+                            confirmBtnText: 'Yes',
+                            cancelBtnText: 'No',
+                            loopAnimation: false,
+                            onConfirmBtnTap: () {
+                              DateTime now = new DateTime.now();
+                              print(now.toString());
+                              print(sampleData[0]);
+                              sentReportData.clear();
+                              sentReportData.add(sampleData[0]);
+                              sentReportData[0].sentRep = now.toString();
+                              sentReportData[0].sentRepStatus = "SENT REPORT";
+                              SentReport();
+                            },
+                            onCancelBtnTap: () {},
+                          );
+                        },
                       ),
-                      onPressed: () {
-                        CoolAlert.show(
-                          barrierDismissible: true,
-                          width: 200,
-                          context: context,
-                          type: CoolAlertType.warning,
-                          text: 'CANCEL SENT REPORT',
-                          confirmBtnText: 'Yes',
-                          cancelBtnText: 'No',
-                          loopAnimation: true,
-                          onConfirmBtnTap: () {
-                            cancelReportData.clear();
-                            cancelReportData.add(sampleData[0]);
-                            cancelReportData[0].sentRep = null;
-                            cancelReportData[0].sentRepStatus = null;
-                            submitcancelsentReport();
-                          },
-                          onCancelBtnTap: () {},
-                        );
-                      },
                     ),
-                  ),
+                  ],
+                  if (requestData[0].sentRepStatus.toString() ==
+                          "SENT COMPLETE" &&
+                      requestData[0].incharge == userName) ...[
+                    Container(
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        icon: Icon(
+                          Icons.cancel,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {
+                          CoolAlert.show(
+                            barrierDismissible: true,
+                            width: 200,
+                            context: context,
+                            type: CoolAlertType.warning,
+                            text: 'CANCEL SENT REPORT',
+                            confirmBtnText: 'Yes',
+                            cancelBtnText: 'No',
+                            loopAnimation: true,
+                            onConfirmBtnTap: () {
+                              cancelReportData.clear();
+                              cancelReportData.add(sampleData[0]);
+                              cancelReportData[0].sentRep = null;
+                              cancelReportData[0].sentRepStatus = null;
+                              submitcancelsentReport();
+                            },
+                            onCancelBtnTap: () {},
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                  if (requestData[0].sentRep.toString() == "SENT COMPLETE") ...[
+                    Text(
+                      toDateTime(requestData[0].sentRep.toString()),
+                    ),
+                  ],
                 ],
-                if (requestData[0].sentRep.toString() == "SENT COMPLETE") ...[
-                  Text(
-                    toDateTime(requestData[0].sentRep.toString()),
-                  ),
-                ],
-              ],
-            ),
+              ),
           ],
         );
       });
